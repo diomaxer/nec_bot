0e655cd036ec8b9e81a5a10897e48ca9179c990d
root <root@ibm-p8-kvm-03-guest-02.virt.pnr.lab.eng.rdu2.redhat.com>
2022-05-17 23:38:17 +0300

    dev version 1.0

--- main.py
+++ main.py
@@ -4,27 +4,27 @@ import random
 import telebot
 
 
-token = 'token'
+token = '5389192597:AAF2-PB6n-EXUUYWEfJImYEbrms0tZzhQNQ'
 
 
 def get_joke():
     file_name = random.choice(os.listdir(os.path.abspath('files')))
     with open('files/' + file_name, 'r', encoding='utf-8') as file:
         dirty_text = file.readlines()
-        clean_text = []
-        joke = ''
-        for item in dirty_text[1:]:
-            if item in ['\n']:
-                if joke not in ['', '\n', '\t']:
-                    clean_text.append(joke)
-                    joke = ''
-                continue
-            if item in ['']:
-                continue
-            else:
-                joke += item
-        message = f'{file_name[:-4]}\n'
-        return message + random.choice(clean_text)
+    clean_text = []
+    joke = ''
+    for item in dirty_text[1:]:
+        if item in ['\n']:
+            if joke not in ['', '\n']:
+                clean_text.append(joke)
+                joke = ''
+            continue
+        if item in ['']:
+            continue
+        else:
+            joke += item
+    message = dirty_text[0]
+    return message + random.choice(clean_text)
 
 
 bot = telebot.TeleBot(token)
@@ -34,7 +34,7 @@ bot = telebot.TeleBot(token)
 def start_message(message):
     while True:
         bot.send_message(message.chat.id, get_joke())
-        time.sleep(10)
+        time.sleep(60*60)
 
 
 bot.infinity_polling()
